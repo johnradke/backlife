033c4b550f40e0bec569a4869aedea82706da8cf
John Radke <jtradke@gmail.com>
2013-04-13 18:23:36 -0500

    Some refactoring, added NAPALM pattern

--- life.js
+++ life.js
@@ -24,18 +24,16 @@ var life = function (canvasId, scale) {
 		else {
 			on = alive;
 		}
-		var val = on ? 0 : 255;
+		var val = on ? 0 : 227;
 		
-		for (xoff = 0; xoff < scale; xoff++) {
-			for (yoff = 0; yoff < scale; yoff++) {
-				var index = getPixelIndex(x * scale + xoff, y * scale + yoff);
-				
-				i.data[index] = val;
-				i.data[index + 1] = val;
-				i.data[index + 2] = val;
-				i.data[index + 3] = 255;
-			}
-		}
+		doBlock(scale, scale, function(xoff, yoff) {
+			var index = getPixelIndex(x * scale + xoff, y * scale + yoff);
+
+			i.data[index] = val;
+			i.data[index + 1] = val;
+			i.data[index + 2] = val;
+			i.data[index + 3] = 255;
+		});
 	};
 
 	var isAlive = function (x, y) {
@@ -49,9 +47,9 @@ var life = function (canvasId, scale) {
 	};
 	
 	var setPattern = function(x, y, pattern) {
-		for (var xoff = 0; xoff < pattern.length; xoff ++) {
-			for (var yoff = 0; yoff < pattern[xoff].length; yoff++) {
-				setAlive(img, x + xoff, y + yoff, pattern[xoff][yoff]);
+		for (var yoff = 0; yoff < pattern.length; yoff ++) {
+			for (var xoff = 0; xoff < pattern[yoff].length; xoff++) {
+				setAlive(img, x + xoff, y + yoff, pattern[yoff][xoff]);
 			}
 		}
 	};
@@ -74,6 +72,7 @@ var life = function (canvasId, scale) {
 	
 	var neighborCount = function(x, y) {
 		var sum = 0;
+		
 		for (var xoff = -1; xoff <= 1; xoff++) {
 			for (var yoff = -1; yoff <=1; yoff++) {
 				if (xoff === 0 && yoff === 0) {
@@ -82,6 +81,7 @@ var life = function (canvasId, scale) {
 				sum += isAlive(x + xoff, y + yoff) ? 1 : 0;
 			}
 		}
+		
 		return sum;
 	};
 	
@@ -91,28 +91,37 @@ var life = function (canvasId, scale) {
 	var aPrevGen = function () {
 		var prev = ctx.createImageData(img);
 		
-		for (var x = 0; x < width; x++) {
-			for (var y = 0; y < height; y++) {
-				
-			}
-		}
-		
+		doBlock(width, height, function(x, y) {
+			
+		});
 		img = prev;
 	};
 	
 	var nextGen = function () {
 		var next = ctx.createImageData(img);
-		for (var x = 0; x < width; x++) {
-			for (var y = 0; y < height; y++) {
-				setAlive(next, x, y, willBeAlive);
-			}
-		}
+		
+		doBlock(width, height, function(x, y) {
+			setAlive(next, x, y, willBeAlive);
+		});
+		
 		img = next;
 	};
 	
 	var draw = function () {
 		ctx.putImageData(img, 0, 0);
 	};
+	
+	var doBlock = function(xmax, ymax, action) {
+		for (var x = 0; x < xmax; x++) {
+			for (var y = 0; y < ymax; y++) {
+				action(x, y);
+			}
+		}
+	};
+	
+	doBlock(width, height, function(x, y) {
+		setAlive(img, x, y, false);
+	});
 	
 	return {
 		setAlive: function(x, y, alive) { setAlive (img, x, y, alive); },
@@ -124,6 +133,7 @@ var life = function (canvasId, scale) {
 		aPrevGen: aPrevGen,
 		setPattern: setPattern,
 		GLIDER: [[0, 1, 0], [0, 0, 1], [1, 1, 1]],
-		GROWTH: [[1, 1, 1, 0, 1], [1, 0, 0, 0, 0], [0, 0, 0, 1, 1], [0, 1, 1, 0, 1], [1, 0, 1, 0, 1]]
+		GROWTH: [[1, 1, 1, 0, 1], [1, 0, 0, 0, 0], [0, 0, 0, 1, 1], [0, 1, 1, 0, 1], [1, 0, 1, 0, 1]],
+		NAPALM: [[0, 1, 0], [1, 0, 1], [1, 0, 1], [1, 0, 1]]
 	};
 };
